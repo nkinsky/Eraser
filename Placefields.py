0add0dc6062ac64fa10f079093156ef5a79783c3
nkinsky <kinsky@bu.edu>
2018-05-25 09:33:28 -0400

    setup function

--- Placefields.py
+++ Placefields.py
@@ -12,9 +12,26 @@ from os import path
 import skvideo.io
 from glob import glob
 from session_directory import find_eraser_directory as get_dir
-from er_plot_functions import get_pos
+import er_plot_functions as er
 
-def MakeOccMap(xcm,ycm,lims,good,isrunning,cmperbin):
+
+def placefields(mouse, arena, day, list_dir='E:\Eraser\SessionDirectories'):
+    """
+
+    :param mouse:
+    :param arena:
+    :param day:
+    :param list_dir:
+    :return:
+    """
+    dir_use = get_dir(mouse, arena, day, list_dir)
+    pos = er.get_pos(dir_use)
+    _, _, pix2cm = er.get_conv_factors(arena)
+    poscm = pos*pix2cm
+    lims = [np.min(poscm,axis=1), np.max(poscm, axis=1)]
+
+
+def makeoccmap(xcm, ycm, lims, good, isrunning, cmperbin):
     """
     Make Occupancy Map
     :param
@@ -22,3 +39,4 @@ def MakeOccMap(xcm,ycm,lims,good,isrunning,cmperbin):
         vidfile: full path to video file
     :return:
     """
+
