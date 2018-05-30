ecfebf2fc1f201a468b68a6d9c35cec68c99ffa6
nkinsky <kinsky@bu.edu>
2018-05-30 08:11:35 -0400

    porting over from MATLAB - need to vet makeplacefield

--- Placefields.py
+++ Placefields.py
@@ -6,37 +6,236 @@ Created on Thu Apr 05 11:06:20 2018
 """
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
+import matplotlib as mp
+import scipy.io as sio
+import scipy.ndimage as sim
+# import csv
 import pandas as pd
 from os import path
-import skvideo.io
-from glob import glob
+# import skvideo.io
+# from glob import glob
 from session_directory import find_eraser_directory as get_dir
 import er_plot_functions as er
+from mouse_sessions import make_session_list
 
 
-def placefields(mouse, arena, day, list_dir='E:\Eraser\SessionDirectories'):
+def placefields(mouse, arena, day, list_dir='E:\Eraser\SessionDirectories', cmperbin=4,
+                lims_method='auto'):
+
     """
-
-    :param mouse:
-    :param arena:
-    :param day:
-    :param list_dir:
+    Make placefields of each neuron
+    :param mouse: mouse name to analyze
+    :param arena: arena to analyze
+    :param day: day to analyze
+    :param list_dir: list alternate sessiondirectories location here
+    :param cmperbin: 4 default
+    :param lims_method: 'auto' (default) takes limits of data, 'manual' looks for arena_lims.csv
+            file in the session directory which supplies [[xmin, ymin],[xmax, ymax]]
     :return:
     """
+
+    make_session_list(list_dir)
+
+    # Get position and time information for .csv file (later need to align to imaging)
     dir_use = get_dir(mouse, arena, day, list_dir)
-    pos = er.get_pos(dir_use)
-    _, _, pix2cm = er.get_conv_factors(arena)
-    poscm = pos*pix2cm
-    lims = [np.min(poscm,axis=1), np.max(poscm, axis=1)]
+    speed, pos, t_track, sr = get_speed(dir_use)
+
+    # Import imaging data
+    im_data_file = path.join(dir_use + '\imaging', 'FinalOutput.mat')
+    im_data = sio.loadmat(im_data_file)
+    PSAbool = im_data['PSAbool']
+    nneurons, _ = np.shape(PSAbool)
+    try:
+        sr_image = im_data['SampleRate']
+    except KeyError:
+        sr_image = 20
+
+        # Convert position to cm
+    _, _, pix2cm = er.get_conv_factors(arena)  # get conversion to cm for the arena
+    pos_cm = pos*pix2cm
+    speed_cm = speed*pix2cm
+
+    # Align imaging and position data
+    pos_align, speed_align, PSAbool_align, PSAoffset, PSoffsetRear, time_interp  = \
+        align_imaging_to_tracking(pos_cm, speed_cm, t_track, PSAbool, sr_image)
+
+    # Smooth speed data for legitimate thresholding, get limits of data
+    speed_sm = np.convolve(speed_align, np.ones(2*int(sr))/(2*sr), mode='same')  # smooth speed
+    lims = [np.min(pos_cm, axis=1), np.max(pos_cm, axis=1)]
+    good = np.ones(len(speed_sm)) == 1
+    isrunning = good
+    isrunning[speed_sm < 1.5] = False
+
+    # Get the mouse's occupancy in each spatial bin
+    occmap, runoccmap, xEdges, yEdges, xBin, yBin = \
+        makeoccmap(pos_align, lims, good, isrunning, cmperbin)
+
+    # Get rid of non-running epochs
+    xrun = pos_align[0, isrunning]
+    yrun = pos_align[1, isrunning]
+    PSAboolrun = PSAbool_align[:, isrunning]
+    xBinrun = xBin[isrunning]
+    yBinrun = yBin[isrunning]
+    nGood = len(xrun)
+
+# Construct place field and compute mutual information
+neurons = list(range(0, nneurons))
+# TCounts = cell(1, nNeurons);
+# TMap_gauss = cell(1, nNeurons);
+# TMap_unsmoothed = cell(1, nNeurons);
+# pos = [x; y];
+for neuron in neurons:
+    tmap_us[neuron], tcounts[neuron], tmap_gauss[neuron] = \
+        makeplacefield(PSAboolrun[neuron, :], xrun, yrun, xEdges, yEdges, runoccmap,
+                       cmperbin=cmperbin)
 
 
-def makeoccmap(xcm, ycm, lims, good, isrunning, cmperbin):
+def makeoccmap(pos_cm, lims, good, isrunning, cmperbin):
     """
-    Make Occupancy Map
-    :param
-        ax: matplotlib.pyplot axes
-        vidfile: full path to video file
+    Make Occupancy map for mouse
+    :param pos_cm:
+    :param lims:
+    :param good:
+    :param isrunning:
+    :param cmperbin:
+    :return: occmap, runoccmap, xEdges, yEdges, xBin, yBin
+    """
+
+    # Extract Limits
+    xmin = lims[0][0]
+    xmax = lims[1][0]
+    ymin = lims[0][1]
+    ymax = lims[1][1]
+
+    # Make edges for hist2
+    xrange = xmax - xmin
+    yrange = ymax - ymin
+
+    nXBins = np.ceil(xrange/cmperbin)
+    nYBins = np.ceil(yrange/cmperbin)
+
+    xEdges = np.linspace(0, nXBins, nXBins)*cmperbin + xmin
+    yEdges = np.linspace(0, nYBins, nYBins)*cmperbin + ymin
+
+    # Run 2d histogram function to get occupancy and running occupancy maps
+    occmap, _, _ = np.histogram2d(pos_cm[0, good], pos_cm[1, good], bins=[xEdges, yEdges])
+    runoccmap, _, _ = np.histogram2d(pos_cm[0, good & isrunning], pos_cm[1, good & isrunning],
+                                     bins=[xEdges, yEdges])
+    xBin = np.digitize(pos_cm[0, :], xEdges)
+    yBin = np.digitize(pos_cm[1, :], yEdges)
+
+    return occmap, runoccmap, xEdges, yEdges, xBin, yBin
+
+
+def get_speed(dir_use):
+    """
+
+    :param dir_use: home directory for the mouse.
+
+    :return: speed and pos (pix/sec, and pix), timestamps for tracking data (s),
+     and sr (sample rate, Hz)
+    """
+
+    # Get position and time information for .csv file (later need to align to imaging)
+    pos = er.get_pos(dir_use)  # pos in pixels
+    t = er.get_timestamps(dir_use)  # time in seconds
+    # Adjust/align to imaging here or above
+    # NK - need to interpolate any positions at 0,0 here and spit out the total number in the output
+
+    # Calculate speed
+    pos_diff = np.diff(pos.T, axis=0)  # For calculating distance.
+    time_diff = np.diff(t)  # Time difference.
+    distance = np.hypot(pos_diff[:, 0], pos_diff[:, 1])  # Displacement.
+    speed = np.concatenate(([0], distance // time_diff[0:-1]))  # Velocity. cm/sec
+    sr = np.round(1 / np.mean(time_diff))
+
+    return speed, pos, t, sr
+
+
+def imshow_nan(array, ax=None, cmap='viridis'):
+    """
+    Plot an array with nan values set as white.
+    Not even sure this is necessary anymore - default might plot nans as white...
+    :param array: array to plot - nans will be white
+    :param ax: optional, if left out or set to None creates a new figure
+    :param cmap: optional, default = viridis
+    :return: ax
+    """
+
+    # Define plotting axes if not specified
+    if ax == None:
+        _, ax = plt.subplots()
+
+    cmap_use = mp.colors.Colormap(cmap)
+    cmap_use.set_bad('white', 1.)
+    ax.imshow(array, cmap=cmap_use)
+
+    # Need to add something here and at the end to set back to original cmap just in case
+
+    return ax
+
+
+def align_imaging_to_tracking(pos_cm, speed_cm, time_tracking, PSAbool, sr_imaging):
+    """
+    Aligns imaging and tracking data and spits out all at the sample rate of the imaging data
+    :param pos_cm:
+    :param time_tracking:
+    :param sr_imaging:
+    :param PSAbool:
+    :return: pos_align, speed_align, PSAbool_align, t_imaging - all aligned to t_imaging
+    """
+
+    # Get timestamps for PSAbool
+    _, nframes = np.shape(PSAbool)
+    t_imaging = np.arange(0, nframes/sr_imaging, 1/sr_imaging) + \
+                np.min(time_tracking)  # tracking software starts image capture
+
+    pos_align = np.interp(t_imaging, time_tracking, pos_cm)
+    speed_align = np.interp(t_imaging, time_tracking, speed_cm)
+
+    # Chop any frames in imaging data that extend beyond tracking data
+    t_im_include_bool = np.max(time_tracking) > t_imaging
+    PSAbool_align = PSAbool[:, t_im_include_bool]
+    pos_align = pos_align[:, t_im_include_bool]
+    speed_align = speed_align[t_im_include_bool]
+
+    return pos_align, speed_align, PSAbool_align, t_imaging
+
+def makeplacefield(PSAbool, x, y, xEdges, yEdges, runoccmap, cmperbin=4, gauss_std=2.5):
+    """
+    Make placefields from aligned imaging/tracking data
+    :param PSAbool:
+    :param x:
+    :param y:
+    :param xEdges:
+    :param yEdges:
+    :param runoccmap:
+    :param cmperbin:
+    :param smooth:
+    :param gauss_std:
     :return:
     """
+    # Get counts of where mouse was for each calcium event in PSAbool
+    tcounts, _, _ = np.histogram2d(x[PSAbool], y[PSAbool], bins=[xEdges, yEdges])
+    tsum = np.sum(tcounts)
 
+    # Normalize it
+    tmap_us = np.divide(tcounts, runoccmap)
+    tmap_us[np.isnan(tmap_us)] = 0
+
+    if tsum != 0:
+        # smooth
+        tmap_sm = sim.filters.gaussian_filter(tmap_us, sigma=gauss_std)  # NK need to vet this
+        tmap_sm = tmap_sm*tsum/np.sum(tmap_sm)
+    elif tsum == 0:  # edge case
+        tmap_sm = tmap_us
+
+    tmap_sm[runoccmap == 0] = np.nan
+    tmap_us[runoccmap == 0] = np.nan
+
+    return tmap_us, tcounts, tmap_sm
+
+
+if __name__ == '__main__':
+    placefields('Marble07', 'Open', -2)
+    pass
