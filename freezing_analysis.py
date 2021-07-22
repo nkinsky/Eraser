9938480b662680da8792515b74a8eebe48051eca
nkinsky <nkinsky@umich.edu>
2021-07-22 07:24:10 -0400

    functions added

--- freezing_analysis.py
+++ freezing_analysis.py
@@ -1,8 +1,17 @@
 import numpy as np
 import er_plot_functions as erp
+import Placefields as pf
 
 
 def align_freezing_to_PSA(PSAbool, sr_image, freezing, video_t):
+    """
+    Align freezing times to neural data.
+    :param PSAbool: nneurons x nframes_imaging boolean ndarray of putative spiking activity
+    :param sr_image: frames/sec
+    :param freezing: output of er_plot_functions.detect_freezing() function.
+    :param video_t: video frame timestamps, same shape as `freezing`
+    :return: freeze_bool: boolean ndarray of shape (nframes_imaging,) indicating frames where animals was freezing.
+    """
 
     # First get imaging parameters and freezing in behavioral video timestamps
     nneurons, nframes = PSAbool.shape
@@ -10,11 +19,73 @@ def align_freezing_to_PSA(PSAbool, sr_image, freezing, video_t):
     freezing_times = [[video_t[epoch[0]], video_t[epoch[1]]] for epoch in freezing_epochs]
 
     # Set up boolean to match neural data shape
-    freezingPSA = np.zeros(nframes, dtype='bool')
+    freeze_bool = np.zeros(nframes, dtype='bool')
     PSAtime = np.arange(0, nframes)/sr_image
 
     # Interpolate freezing times in video time to imaging time
     for freeze_time in freezing_times:
-        freezingPSA[np.bitwise_and(PSAtime >= freeze_time[0], PSAtime < freeze_time[1])] = True
+        freeze_bool[np.bitwise_and(PSAtime >= freeze_time[0], PSAtime < freeze_time[1])] = True
 
-    return freezingPSA
+    return freeze_bool
+
+
+def freeze_event_rate(PSAbool, freeze_bool):
+    """
+    Calculate event rate during freezing times only.
+    :param PSAbool: nneurons x nframes_imaging boolean ndarray of putative spiking activity
+    :param freeze_bool: boolean ndarray of shape (nframes_imaging,) indicating frames where animals was freezing.
+    Get from function `align_freezing_to_PSA`.
+    :return: event_rate_freezing: ndarray of each neuron's event rate during freezing epochs.
+    """
+    event_rate_freezing = PSAbool[:, freeze_bool].sum(axis=1) / freeze_bool.sum()
+
+    return event_rate_freezing
+
+
+def move_event_rate(PSAbool, freeze_bool):
+    """
+        Calculate event rate during motinon (non-freezing) times only.
+        :param PSAbool: nneurons x nframes_imaging boolean ndarray of putative spiking activity
+        :param freezingPSA: boolean ndarray of shape (nframes_imaging,) indicating frames where animals was freezing.
+        Get from function `align_freezing_to_PSA`.
+        :return: event_rate_moving: ndarray of each neuron's event rate during freezing epochs.
+        """
+    event_rate_moving = PSAbool[:, np.bitwise_not(freeze_bool)].sum(axis=1) / np.bitwise_not(freeze_bool).sum()
+
+    return event_rate_moving
+
+
+def motion_modulation_index(mouse, arena, day, **kwargs):
+    """ Calculate motion modulation index (MMI): difference/sum of event rates during motion and freezing (1 = only
+    active during motion, -1 = only active during freezing)
+
+    :param mouse: str
+    :param arena: str ('Shock', 'Open')
+    :param day: int (-2, -1, 0, 4, 1, 2, 7)
+    :param kwargs: see er_plot_functions.detect_freezing() for relevant arguments, most notably `velocity_threshold` and
+    `min_freeze_duration`
+    :return:
+    """
+    # First get directory and neural data
+    dir_use = erp.get_dir(mouse, arena, day)
+    PF = pf.load_pf(mouse, arena, day)
+
+    # Now get behavioral timestamps and freezing times
+    video_t = erp.get_timestamps(str(dir_use))
+    freezing, velocity = erp.detect_freezing(str(dir_use), arena=arena, **kwargs)
+    video_t = video_t[:-1]  # Chop off last timepoint to make this the same length as freezing and velocity arrays
+
+    # Now align freezing to neural data!
+    freeze_bool = align_freezing_to_PSA(PF.PSAbool_align, PF.sr_image, freezing, video_t)
+
+    # Get moving and freezing event rates and calculate MMI
+    event_rate_moving = move_event_rate(PF.PSAbool_align, freeze_bool)
+    event_rate_freezing = freeze_event_rate(PF.PSAbool_align, freeze_bool)
+    MMI = (event_rate_moving - event_rate_freezing) / (event_rate_moving + event_rate_freezing)
+
+    return MMI
+
+if __name__ == '__main__':
+    motion_modulation_index('Marble07', 'Shock', -2)
+
+    pass
