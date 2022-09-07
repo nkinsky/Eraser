90a5337e279355cc6ceca949866812544f10e48d
nkinsky <nkinsky@umich.edu>
2022-09-07 11:38:28 -0400

    enhancement - plots more customizable

--- cell_tracking.py
+++ cell_tracking.py
@@ -6,7 +6,6 @@ Created on Wed Jan 23 11:59:20 2019
 """
 
 import numpy as np
-from numpy.matlib import repmat
 import matplotlib.pyplot as plt
 import matplotlib as mp
 import scipy.io as sio
@@ -68,30 +67,41 @@ def get_group_num_neurons(mice, days=[-2, -1, 4, 1, 2, 7], arenas=['Shock', 'Ope
     return nneurons
 
 
-def plot_num_neurons(nneurons, arena1='Shock', arena2='Open',
-                     day_labels=['-2', '-1', '4hr', '1', '2', '7'], normalize=False):
+def plot_num_neurons(nneurons, arena1='Shock', arena2='Open', day_labels=('-2', '-1', '4hr', '1', '2', '7'),
+                     normalize=False, jitter=(-0.05, 0.05), colors = ('b', 'r'), ax=None, **kwargs):
     """
     Plots # neurons active in each arena on a given day. For eraser but could be used elsewhere
     :param nneurons: nmice x ndays x narenas array. arena1/2 = shock/open by default
     :param arena1/2: arena labels default1/2 = 'Shock'/'Open'.
     :param normalize: boolean False(default) = do not normalize, otherwise takes a
         string = day to normalize to, e.g. '-1' or '7'
+    :param kwargs: inputs to matplotlib.pyplot.plot
     :return: fig, ax
     """
 
     nmice, narenas, ndays = nneurons.shape
-    fig, ax = plt.subplots()
+
+    jitter = (jitter,) if isinstance(jitter, float) else jitter
+    assert len(jitter) == narenas, 'Must enter jitter amount for each arena'
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
 
     # normalize nneurons to day indicated
     if normalize is not False:
         norm_sesh_ind = [day_labels.index(i) for i in day_labels if normalize in i][0]
         nneurons = norm_num_neurons(nneurons, norm_sesh_ind)
 
-    ax.plot(np.matlib.repmat(np.arange(0, ndays), nmice, 1), nneurons[:, 0, :], 'bo')
-    lineshock, = ax.plot(np.arange(0, ndays), np.nanmean(nneurons[:, 0, :], axis=0), 'b-')
+    ax.plot(np.matlib.repmat(np.arange(0, ndays), nmice, 1) + jitter[0], nneurons[:, 0, :],
+            color=colors[0], marker='o', linestyle='None', **kwargs)
+    lineshock, = ax.plot(np.arange(0, ndays) + jitter[0], np.nanmean(nneurons[:, 0, :], axis=0),
+                         color=colors[0], marker=None, linestyle='-', **kwargs)
     if narenas == 2:
-        ax.plot(np.matlib.repmat(np.arange(0, ndays), nmice, 1), nneurons[:, 1, :], 'ro')
-        lineopen, = ax.plot(np.arange(0, ndays), np.nanmean(nneurons[:, 1, :], axis=0), 'r-')
+        ax.plot(np.matlib.repmat(np.arange(0, ndays), nmice, 1) + jitter[1], nneurons[:, 1, :],
+                color=colors[1], marker='o', linestyle='None', **kwargs)
+        lineopen, = ax.plot(np.arange(0, ndays) + jitter[1], np.nanmean(nneurons[:, 1, :], axis=0),
+                            color=colors[1], marker=None, linestyle='-', **kwargs)
         plt.legend((lineshock, lineopen), (arena1, arena2))
 
     if normalize is False:
