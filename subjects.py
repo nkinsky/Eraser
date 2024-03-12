7c75610f09f3ba1d06ece1caaa05527001b6a33c
nkinsky <nkinsky@umich.edu>
2024-03-12 18:05:32 -0400

    initial commit

---
+++ subjects.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+group_data_dir = Path("/Users/nkinsky/Documents/BU/Working/Eraser")
+
+
+def save_df(df: pd.DataFrame, savename: str, save_dir=group_data_dir):
+    """data = pd.DataFrame typically"""
+    assert isinstance(df, pd.DataFrame)
+
+    savename = savename if savename.split(".")[-1] == "csv" else f"{savename}.csv"
+    df.to_csv(save_dir / savename, index=False)
+    print(f"{savename} saved")
+
+
+def load_df(savename: str, save_dir=group_data_dir):
+    savename = savename if savename.split(".")[-1] == "csv" else f"{savename}.csv"
+    return pd.read_csv(save_dir / savename)
